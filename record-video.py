79c042edeac1a04e0db1549e3b8eeb49573b273a
xhmyjae <tomatis.mt@gmail.com>
2023-04-10 22:12:48 +0200

    feat: standard dimension for record video

---
+++ record-video.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2
+
+file_name = 'video.avi'
+frames_per_second = 24.0
+res = '720p'
+
+
+def change_res(cap, width, height):
+    cap.set(3, width)
+    cap.set(4, height)
+
+
+STD_DIMENSIONS =  {
+    "480p": (640, 480),
+    "720p": (1280, 720),
+    "1080p": (1920, 1080),
+    "4k": (3840, 2160),
+}
+
+
+def get_dimensions(cap, res='1080p'):
+    width, height = STD_DIMENSIONS["480p"]
+    if res in STD_DIMENSIONS:
+        width, height = STD_DIMENSIONS[res]
+
+cap = cv2.VideoCapture(0)
+
+while True :
+    ret, frame = cap.read()
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
