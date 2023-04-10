c2e1fcb4a8e8e8cdaa738d3d3882cddd3e6bf5e3
xhmyjae <tomatis.mt@gmail.com>
2023-04-10 20:04:26 +0200

    docs: correct division spaces

--- change-resolution.py
+++ change-resolution.py
@@ -27,8 +27,8 @@ def change_res(width, height):
 
 
 def rescale_frame(frame, percent=75):
-    width = int(frame.shape[1] * percent/ 100)
-    height = int(frame.shape[0] * percent/ 100)
+    width = int(frame.shape[1] * percent / 100)
+    height = int(frame.shape[0] * percent / 100)
     dim = (width, height)
     return cv2.resize(frame, dim, interpolation =cv2.INTER_AREA)
 
