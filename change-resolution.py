b0e9858ad8ba6155abf7c4a963484c50016c58d3
xhmyjae <tomatis.mt@gmail.com>
2023-04-10 20:03:20 +0200

    feat: rescale feature

--- change-resolution.py
+++ change-resolution.py
@@ -26,13 +26,18 @@ def change_res(width, height):
     cap.set(4, height)
 
 
-make_720p()
-change_res(1280, 720)
+def rescale_frame(frame, percent=75):
+    width = int(frame.shape[1] * percent/ 100)
+    height = int(frame.shape[0] * percent/ 100)
+    dim = (width, height)
+    return cv2.resize(frame, dim, interpolation =cv2.INTER_AREA)
+
 
 while True:
     ret, frame = cap.read()
-
+    frame2 = rescale_frame(frame, percent=50)
     cv2.imshow('frame', frame)
+    cv2.imshow('frame2', frame2)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
